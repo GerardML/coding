cf479ce35dde4c1b06b971f12137eba8d663f524
gerardml <menesesgerard@gmail.com>
2021-05-30 15:38:16 -0500

    2nd comm

---
+++ c/printf/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+
+	char a[15],b[15];
+	printf("Ingresa tu nombre: ");
+	scanf("%s",a);
+	printf("Ingresa tu apellido: ");
+	scanf("%s",b);
+
+	printf("\n%s %s es tu nombre completo.\n",a,b);
+	getch();
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
