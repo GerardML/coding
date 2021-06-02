f4b9a74deb2793d3b2f78946f938b142ce26326c
gerardml <menesesgerard@gmail.com>
2021-06-02 13:37:38 -0500

    ++python

--- python/helloworld/main.py
+++ python/helloworld/main.py
@@ -1,3 +1,25 @@
-a = 2
-b = 3
-print("\n + = ",a,b,a+b)
+nombre = "Gerardo Meneses"
+edad = 20
+
+#comentario 1
+"""
+comentario 2
+comentario 3
+"""
+#############____PRINT____##################
+print("Hola Mundo #1")
+#print(f"Var 1 Name: {nombre} Edad: {str(edad)}")
+print("Nombre: " + nombre + ", Edad: " + str(edad))
+nombre = 20
+edad = "Gerardo Meneses"
+#print(f"Var 2 Name: {nombre}, Edad: {str(edad)}")
+print("Nombre: " + str(nombre) + ", Edad: " + edad)
+
+#############____INPUT____##################
+estatura = int(input("Ingrese tu ESTATURA: "))
+
+if estatura >=180:
+    print("Eres una persona alta")
+else:
+    print("Eres una persona bajita")
+    
