d20a4626865ba784aeea82398b3b26855263b562
gerardml <menesesgerard@gmail.com>
2021-06-03 12:18:41 -0500

    Calculator working 100% Windows/Linux

--- c/functions-call/main_linux.c
+++ c/functions-call/main_linux.c
@@ -1,9 +1,55 @@
 #define  suma(a) a[0] + a[1]
 #define resta(a) a[0] - a[1]
 #define multi(a) a[0] * a[1]
-#include<stdio.h>
+#include <stdio.h>
 
+void again();
+void caso_option(char *caso);
+void message(int *a);
+    
+int main(){
+    int a[2];
+    char caso;
+    
+    //printf("\nagain: %c",again);
+    printf("\nBIENVENIDO A CALCULADORA, LAS OPCIONES A CONTINUACION\n");
+        
+        caso_option(&caso);
+        switch (caso)
+        {
+        case 'a':
+            message(a);
+            printf("El resultado es: %d\n",suma(a));
+            break;
+        case 'b':
+            message(a);
+            printf("El resultado es: %d\n",resta(a));
+            break;
+        case 'c':
+            message(a);
+            printf("El resultado es: %d\n",multi(a));
+            break;
+        
+        default: printf("Valor no valido.\n");
+            break;
+        }
+        
+        again();
 
+    return 0;
+}
+
+void again(){
+    char again;
+    printf("\nDesea hacer otra operacion? -> si = y : ");
+    scanf("%c", &again);
+    if (again == 'Y' || again == 'y') main();
+    else
+    {
+        scanf("%c", &again);
+        if (again == 'Y' || again == 'y') main();
+    }
+}
 
 void caso_option(char *caso){
     printf("a) SUMA\n");
@@ -11,6 +57,9 @@ void caso_option(char *caso){
     printf("c) MULTIPLICACION\n");
     printf("Que desea realizar?: ");
     *caso = getchar();
+    if(*caso != 'a' && *caso != 'b' && *caso != 'c'){ //necesario ya que al llamar a la funcion de nuevo "*caso" modifica su valor
+        *caso = getchar();
+    }
 }
 
 void message(int *a){
@@ -26,38 +75,4 @@ void message(int *a){
     scanf("%d",a);
     printf("Ingrese valor #2: ");
     scanf("%d",&a[1]);*/
-}
-
-int main(){
-    int again;
-    int a[2];
-    char caso;
-    
-    printf("\nBIENVENIDO A CALCULADORA, LAS OPCIONES A CONTINUACION\n");
-        
-        caso_option(&caso);
-        switch (caso)
-        {
-        case 'a':
-            message(a);
-            printf("El resultado es: %d\n\n",suma(a));
-            break;
-        case 'b':
-            message(a);
-            printf("El resultado es: %d\n\n",resta(a));
-            break;
-        case 'c':
-            message(a);
-            printf("El resultado es: %d\n\n",multi(a));
-            break;
-        
-        default: printf("Valor no valido.\n");
-            break;
-        }
-
-        printf("Desea hacer otra operacion? -> si = 1 : ");
-	scanf("%d",&again);
-        if(again == 1) main();
-
-    return 0;
 }
